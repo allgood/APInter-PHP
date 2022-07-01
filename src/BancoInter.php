1ae90a5372842f320ad49b14bd3b26ef5589c175
Rodrigo Kravetz de Oliveira <kravetzpm@gmail.com>
2022-07-01 16:41:11 -0300

    compatibilidade PHP 7.4 (#39)

--- src/BancoInter.php
+++ src/BancoInter.php
@@ -517,8 +517,11 @@ class BancoInter
      * @param \DateTime $dataSaldo
      * @return float
      */
-    public function getSaldo(\DateTime $dataSaldo = new \DateTime()): ?float
+    public function getSaldo(\DateTime $dataSaldo = null): ?float
     {
+        if (!$dataSaldo) {
+            $dataSaldo = new \DateTime();
+        }
 
         $reply = $this->controllerGet("/banking/v2/saldo?dataSaldo=" . $dataSaldo->format('Y-m-d'));
         $replyData = json_decode($reply->body);
